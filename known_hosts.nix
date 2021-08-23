5c31d44312b9921a8e648dd69cfa0e5a0f15baf5
Alexander Rezvov <rezvov@majordomo.ru>
2021-08-23 14:00:55 +0300

    Add fluentd, pop5 and chef-server to known_hosts

--- known_hosts.nix
+++ known_hosts.nix
@@ -376,4 +376,19 @@
     publicKeyFile = writeText "webmail3.pub"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF4x35Z4qnL+uribDncs9aQOMAMOqKmjGE9ugDLEw3Be";
   };
+  pop5 = {
+    hostNames = [ "pop5.intr" ];
+    publicKeyFile = writeText "pop5.pub"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJe8vsFmHT8llRa+6IkauSZoursCYbRxOk3Yeyr879pE"
+  };
+  chef-server = {
+    hostNames = [ "chef-server.intr" ];
+    publicKeyFile = writeText "chef-server.pub"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEio+hXB7WrQ8zxCSQXstZv9fL/23MHxZ4Tg4dMs38b"
+  };
+  fluentd = {
+    hostNames = [ "fluentd.intr" ];
+    publicKeyFile = writeText "fluentd.pub"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjaT3qWzQ74bXfqf8XhJE7bWi0Q7M+CAp4BGD1JXNGx"
+  };
 }
