a6ff78f97ec3857e8c39e520fcd8b314a3ece925
Oleg Pykhalov <go.wigust@gmail.com>
2022-11-26 03:17:52 +0300

    Fix Prometheus node exporters interfaces.

--- nixos/modules/services/monitoring/prometheus/exporters/prometheus-node.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/prometheus-node.nix
@@ -8,7 +8,11 @@ let
           (name: _: builtins.match "(([[:alpha:]]\|-)*253+.*)" name != null)
           config.networking.interfaces;
     in
-      (head (head (attrValues interfacesVlan253)).ipv4.addresses).address;
+      if interfacesVlan253 == {}
+      then
+        "0.0.0.0"
+      else
+        (head (head (attrValues interfacesVlan253)).ipv4.addresses).address;
 in {
   services.prometheus.exporters = {
     node = {
